5b1b2882e70812eae3637c4174961623c0243ddb
Tomas Zurita Sorgetti <tomaszuritasorgetti@gmail.com>
2023-10-16 16:50:56 -0300

    sorting criteria added

--- script.py
+++ script.py
@@ -2,3 +2,15 @@ import sorts
 import utils
 
 bookshelf = utils.load_books('bookshelf.csv')
+
+def by_title_ascending(book_a, book_b):
+  return book_a["title_lower"] > book_b["title_lower"]
+
+def by_title_descending(book_a, book_b):
+  return book_a["title_lower"] < book_b["title_lower"]
+
+def by_author_ascending(book_a, book_b):
+  return book_a["author_lower"] > book_b["author_lower"]
+
+def by_author_descending(book_a, book_b):
+  return book_a["author_lower"] < book_b["author_lower"]
