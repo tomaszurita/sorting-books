141f11f97d69c4b1186a525495542cb13836ee38
Tomas Zurita Sorgetti <tomaszuritasorgetti@gmail.com>
2023-10-17 13:15:40 -0300

    final script

--- script.py
+++ script.py
@@ -18,3 +18,30 @@ def by_author_ascending(book_a, book_b):
 # Sorts bookshelf by author Z-A
 def by_author_descending(book_a, book_b):
   return book_a["author_lower"] < book_b["author_lower"]
+
+# Creates a copy of the bookshelf in which the sorting will happen
+bookshelf_copy = bookshelf.copy()
+
+# Sorts by title A-Z using bubble sort algorithm
+#bubble_sort_by_title_a_z = sorts.bubble_sort(bookshelf_copy, by_title_ascending)
+# Sorts by title A-Z using quicksort algorithm
+quicksort_by_title_a_z = sorts.quicksort(bookshelf_copy, 0, len(bookshelf_copy) - 1, by_title_ascending)
+
+# Sorts by title Z-A using bubble sort algorithm
+#bubble_sort_by_title_z_a = sorts.bubble_sort(bookshelf_copy, by_title_descending)
+# Sorts by title Z-A using quicksort algorithm
+#quicksort_by_title_z_a = sorts.quicksort(bookshelf_copy, 0, len(bookshelf_copy) - 1, by_title_descending)
+
+# Sorts by author A-Z using bubble sort algorithm
+#bubble_sort_by_author_a_z = sorts.bubble_sort(bookshelf_copy, by_author_ascending)
+# Sorts by author A-Z using quicksort algorithm
+#quicksort_by_author_a_z = sorts.quicksort(bookshelf_copy, 0, len(bookshelf_copy) - 1, by_author_ascending)
+
+# Sorts by author Z-A using bubble sort algorithm
+#bubble_sort_by_author_z_a = sorts.bubble_sort(bookshelf_copy, by_author_descending)
+# Sorts by author Z-A using quicksort algorithm
+#quicksort_by_author_z_a = sorts.quicksort(bookshelf_copy, 0, len(bookshelf_copy) - 1, by_author_descending)
+
+# Displays the sorted list
+for book in bookshelf_copy:
+    print(book["title"] + ", " + book["author"])
